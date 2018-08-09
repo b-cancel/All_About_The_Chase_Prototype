e109a9d2ce65b95817fde57086ca76ea7b0bbecd
Bryan Cancel <bryan.o.cancel@gmail.com>
2018-08-09 00:17:28 -0500

    sorted which one of the 3 is newest, I have yet to refine the system and test the pairing kit with it

--- All_About_The_Chase_Prototype_2/Assets/Scripts/misc/Tile.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Tile {
-    public FloorData floorData;
-    public WallData wallData;
-    public string type;
-}
