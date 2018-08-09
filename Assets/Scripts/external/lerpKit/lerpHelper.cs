f2ceda28371ab20e6543e869d7dab66db68c65bb
Bryan Cancel <bryan.o.cancel@gmail.com>
2018-08-09 17:27:13 -0500

    super basic working build, additions to lerpkit required that work with Mathf.LerpAngle and it seems like repairs to pairing kit are required for negative numbers

--- Assets/Scripts/external/lerpKit/lerpHelper.cs
+++ Assets/Scripts/external/lerpKit/lerpHelper.cs
@@ -2,8 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//last updated: 8/5/18
+
 namespace lerpKit
 {
+    //  This toolKit can help you pass an appropiate 't' value for most functions that have a lerp (or linear interpolation) function
+    //      and allow you to properly perform a linear interpolation
+    //  Like: Mathf.Lerp, Vector2.Lerp, Vector3.Lerp, Vector4.Lerp, Color.Lerp, and Color32.Lerp
+    //  But Not: 
+    //  (1) Material.Lerp: it seems like you can do this by messing with only 1. Material.color 2. Material.mainTextureOffset and 3. Material.mainTextureScale
+    //  (2) Quaternion.Lerp: this has no simple definition of "distance" and therefore I can not generate a lerpVelocity
+    //  (3) Math.LerpAngle: i was not able to figure out how this work internally because in my version of unity its possible to get values greater than 360 and smaller than 0, which seems to go against its definition
+    //  NOTE: Mathf.MoveTowards, Vector2.MoveTowards, Vector3.MoveTowards, Vector4.MoveTowards, and Mathf.MoveTowardsAngle might be better suited to your needs
+
     public static class lerpHelper
     {
         //-------------------------CALCULATE GUIDE DISTANCE-------------------------
@@ -11,13 +22,12 @@ namespace lerpKit
         /// <summary>
         /// You only need to fill in the values that guide distance is asking for
         /// the other parameter must be filled in but will not affect the result
+        /// NOTE: guideDistance.other has no definition for anything but color
         /// EX: IF (your passed GD == guideDistance.distBetween_StartAndEnd) -> currValue(s) will not be used
         ///     because GD is only asking for a startValue(s) and endValue(s)
         /// </summary>
-        public static float calcGuideDistance(float startValue, float currValue, float endValue, guideDistance GD) 
+        public static float calcGuideDistance(float startValue, float currValue, float endValue, guideDistance GD)
         {
-            //NOTE: guideDistance.other has no definition for anything but color
-
             if (GD == guideDistance.distBetween_StartAndCurr)
                 return Mathf.Abs(startValue - currValue);
             else if (GD == guideDistance.distBetween_StartAndEnd)
@@ -29,13 +39,12 @@ namespace lerpKit
         /// <summary>
         /// You only need to fill in the values that guide distance is asking for
         /// the other parameter must be filled in but will not affect the result
+        /// NOTE: guideDistance.other has no definition for anything but color
         /// EX: IF (your passed GD == guideDistance.distBetween_StartAndEnd) -> currValue(s) will not be used
         ///     because GD is only asking for a startValue(s) and endValue(s)
         /// </summary>
         public static float calcGuideDistance(Vector2 startVect2, Vector2 currVector2, Vector2 endVector2, guideDistance GD)
         {
-            //NOTE: guideDistance.other has no definition for anything but color
-
             if (GD == guideDistance.distBetween_StartAndCurr)
                 return Vector2.Distance(startVect2, currVector2);
             else if (GD == guideDistance.distBetween_StartAndEnd)
@@ -47,13 +56,12 @@ namespace lerpKit
         /// <summary>
         /// You only need to fill in the values that guide distance is asking for
         /// the other parameter must be filled in but will not affect the result
+        /// NOTE: guideDistance.other has no definition for anything but color
         /// EX: IF (your passed GD == guideDistance.distBetween_StartAndEnd) -> currValue(s) will not be used
         ///     because GD is only asking for a startValue(s) and endValue(s)
         /// </summary>
         public static float calcGuideDistance(Vector3 startVect3, Vector3 currVector3, Vector3 endVector3, guideDistance GD)
         {
-            //NOTE: guideDistance.other has no definition for anything but color
-
             if (GD == guideDistance.distBetween_StartAndCurr)
                 return Vector3.Distance(startVect3, currVector3);
             else if (GD == guideDistance.distBetween_StartAndEnd)
@@ -65,13 +73,12 @@ namespace lerpKit
         /// <summary>
         /// You only need to fill in the values that guide distance is asking for
         /// the other parameter must be filled in but will not affect the result
+        /// NOTE: guideDistance.other has no definition for anything but color
         /// EX: IF (your passed GD == guideDistance.distBetween_StartAndEnd) -> currValue(s) will not be used
         ///     because GD is only asking for a startValue(s) and endValue(s)
         /// </summary>
         public static float calcGuideDistance(Vector4 startVect4, Vector4 currVector4, Vector4 endVector4, guideDistance GD)
         {
-            //NOTE: guideDistance.other has no definition for anything but color
-
             if (GD == guideDistance.distBetween_StartAndCurr)
                 return Vector4.Distance(startVect4, currVector4);
             else if (GD == guideDistance.distBetween_StartAndEnd)
@@ -83,13 +90,12 @@ namespace lerpKit
         /// <summary>
         /// You only need to fill in the values that guide distance is asking for
         /// the other parameter must be filled in but will not affect the result
+        /// NOTE: guideDistance.other has no definition for anything but color
         /// EX: IF (your passed GD == guideDistance.distBetween_StartAndEnd) -> currValue(s) will not be used
         ///     because GD is only asking for a startValue(s) and endValue(s)
         /// </summary>
         public static float calcGuideDistance(float[] startValues, float[] currValues, float[] endValues, guideDistance GD)
         {
-            //NOTE: guideDistance.other has no definition for anything but color
-
             if (GD == guideDistance.distBetween_StartAndCurr)
                 return euclideanDistance(startValues, currValues);
             else if (GD == guideDistance.distBetween_StartAndEnd)
@@ -101,6 +107,7 @@ namespace lerpKit
         /// <summary>
         /// You only need to fill in the values that guide distance is asking for
         /// the other parameter must be filled in but will not affect the result
+        /// NOTE: guideDistance.other has no definition for anything but color
         /// EX: IF (your passed GD == guideDistance.distBetween_StartAndEnd) -> currValue(s) will not be used
         ///     because GD is only asking for a startValue(s) and endValue(s)
         /// </summary>
@@ -117,39 +124,7 @@ namespace lerpKit
                 return 441.672956f; // maxDistanceInRGBColorSpace
         }
 
-        //-------------------------CALCULATE LERP VALUE (using Guide Distance, Guide Time, Unit of Time, and Update Location)-------------------------
-
-        public static float calcLerpValue(float currValue, float endValue, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-        {
-            return calcLerpValue(currValue, endValue, calcLerpVelocity(guideDistance, guideTime, UOT_GD, UL));
-        }
-
-        public static float calcLerpValue(Vector2 currVector2, Vector2 endVector2, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-        {
-            return calcLerpValue(currVector2, endVector2, calcLerpVelocity(guideDistance, guideTime, UOT_GD, UL));
-        }
-
-        public static float calcLerpValue(Vector3 currVector3, Vector3 endVector3, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-        {
-            return calcLerpValue(currVector3, endVector3, calcLerpVelocity(guideDistance, guideTime, UOT_GD, UL));
-        }
-
-        public static float calcLerpValue(Vector4 currVector4, Vector4 endVector4, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-        {
-            return calcLerpValue(currVector4, endVector4, calcLerpVelocity(guideDistance, guideTime, UOT_GD, UL));
-        }
-
-        public static float calcLerpValue(float[] currValues, float[] endValues, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-        {
-            return calcLerpValue(currValues, endValues, calcLerpVelocity(guideDistance, guideTime, UOT_GD, UL));
-        }
-
-        public static float calcLerpValue(Color currColor, Color endColor, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-        {
-            return calcLerpValue(currColor, endColor, calcLerpVelocity(guideDistance, guideTime, UOT_GD, UL));
-        }
-
-        //-------------------------CALCULATE LERP VALUE (using Guide Velocity)-------------------------
+        //-------------------------CALCULATE LERP VALUE-------------------------
 
         public static float calcLerpValue(float currValue, float endValue, float lerpVelocity_DperF)
         {
@@ -212,6 +187,7 @@ namespace lerpKit
             return calcLerpVelocity(guideDistance, timeToFrames(timeToTravel_GD, UOT_GD, UL));
         }
 
+        //NOTE: this is public only so we can make our extension methods work
         public static float calcLerpVelocity(float guideDistance, float framesToTravel_GD) //frames of this type (update if we are in update... fixed update if we are in fixed upate)
         {
             return guideDistance / framesToTravel_GD;
@@ -219,6 +195,7 @@ namespace lerpKit
 
         //-------------------------HELPER FUNCTIONS-------------------------
 
+        //NOTE: this is public only so we can make our extension methods work
         public static float timeToFrames(float time, unitOfTime UOT, updateLocation UL) //Prefably calculate velocity and dont use this function directly
         {
             if (UOT == unitOfTime.frames)
@@ -252,7 +229,7 @@ namespace lerpKit
 
         public static float distance(Color color1, Color color2) //found in colorKit, colorDistances class [THIS IS MODIFIED]
         {
-            float[] color1_Float_rGb = color_to_array(color1); 
+            float[] color1_Float_rGb = color_to_array(color1);
             float[] color1_255_rGb = _float_to_255(color1_Float_rGb);
 
             float[] color2_Float_rGb = color_to_array(color2);
@@ -270,7 +247,7 @@ namespace lerpKit
 
         static Vector3 array_to_vector3(float[] array) //found in extraKit, formatConversion class [THIS IS MODIFIED]
         {
-                return new Vector3(array[0], array[1], array[2]);
+            return new Vector3(array[0], array[1], array[2]);
         }
 
         static float[] color_to_array(Color color) //found in extraKit, formatConversion class
