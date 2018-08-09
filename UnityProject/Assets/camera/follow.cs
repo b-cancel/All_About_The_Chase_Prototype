6f9ab65731d2dbb76c6322cc11b845e4596777bc
Bryan Cancel <bryan.o.cancel@gmail.com>
2018-08-09 18:47:51 -0500

    updated lerp kit with exported build runnable on web player

---
+++ UnityProject/Assets/camera/follow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using lerpKit;
+
+public class follow : MonoBehaviour {
+
+    public GameObject objectToFollow;
+
+    float rotationVelocity;
+
+    private void Start()
+    {
+        float guideDistance = 360f;
+        float guideTime = 1f;
+        rotationVelocity = lerpHelper.calcLerpVelocity(guideDistance, guideTime, unitOfTime.seconds, updateLocation.update);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 objectPos = objectToFollow.transform.position;
+        gameObject.transform.position = new Vector3(objectPos.x, objectPos.y, gameObject.transform.position.z);
+
+        Vector3 objectRot = objectToFollow.transform.rotation.eulerAngles;
+        Vector3 ourRot = gameObject.transform.rotation.eulerAngles;
+
+        float newLerp = lerpHelper.calcLerpValueAngle(ourRot.z, objectRot.z, rotationVelocity, wrapType.shortest);   
+        gameObject.transform.rotation = Quaternion.Euler(ourRot.x, ourRot.y, Mathf.LerpAngle(ourRot.z, objectRot.z, newLerp));
+	}
+}
